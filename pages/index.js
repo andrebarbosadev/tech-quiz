761a3e1528478939018a789966a044733f89a13d
ANDRE DA SILVA BARBOSA <andredsb@ITAIPU.INT>
2021-01-27 16:54:48 -0300

    Reorganization of components.

--- pages/index.js
+++ pages/index.js
@@ -10,24 +10,9 @@ import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
   const router = useRouter();
@@ -50,18 +35,15 @@ export default function Home() {
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <input
-                onChange={function (event) {
-                  setName(event.target.value);
-                }}
-                placeholder=""
-                id="name"
+              <Input
+                onChange={(event) => { setName(event.target.value); }}
+                placeholder="Qual o seu nome?"
+                name="nomeDoUsuario"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {' '}
-                {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,10 +1,62 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
+import styled from 'styled-components';
+
+import Head from 'next/head';
+import { useRouter, useState } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
+
+function LoadingWidget() {
+    return(
+      <Widget>
+        <Widget.Header>
+          Carregando
+        </Widget.Header>
+        <Widget.Content>
+          [Desafio Tech]
+        </Widget.Content>
+      </Widget>
+    );
+}
 
 export default function QuizPage() {
   return (
-    <div>
-      Página de Quiz!
-    </div>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Ultimate Tech Quiz!</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            Quiz Tech!
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            Demais quizes criados!
+          </Widget.Content>
+        </Widget>
+        <LoadingWidget/>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/andrebarbosafoz" />
+    </QuizBackground>
   );
 }
